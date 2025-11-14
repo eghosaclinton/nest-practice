b1c59573ee41984cc85a5bd90eb598c5966081aa
Aisosa <114480333+eghosaclinton@users.noreply.github.com>
2025-11-14 10:24:54 +0100

    added a global validation pipe and added some decorators to the DTOs

--- src/profiles/dto/update-profile.dto.ts
+++ src/profiles/dto/update-profile.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, Length, Contains } from 'class-validator';
+
 export class UpdateProfileDto {
+  @IsString()
+  @Length(3, 100)
   name: string;
+
+  @IsString()
+  @Contains('software')
   description: string;
 }
